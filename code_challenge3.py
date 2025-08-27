60bab211619c60eb4ee9d0a0cc27f9488394e57e
aizelendozo08-cyber <aizelendozo08@gmail.com>
2025-08-27 11:24:42 +0800

    Add files via upload

---
+++ code_challenge3.py
@@ -0,0 +1,10 @@
+username = 'Aizel'
+pwd= 'ayokona'
+
+username = input ('Enter username: ')
+password = input ('Enter password: ')
+
+if username == 'Aizel' and 'ayokona' == 'ayokona' :
+	print ("Access Granted! ")
+else:
+	print ("Access Denied. ")
