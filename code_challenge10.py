932924af7f080a72d827ac424d06024f290850de
aizelendozo08-cyber <aizelendozo08@gmail.com>
2025-09-23 15:02:42 +0800

    Add files via upload

---
+++ code_challenge10.py
@@ -0,0 +1,9 @@
+print("\t\t *", end="" )
+for aizel in range(1, 11, 1):
+    for x in range(10, aizel, -1):
+        print(" ", end=" ")
+    for f in range(1, aizel, 1):
+        print("*", end=" ")
+    for e in range(1, aizel, 1):
+        print("*", end=" ")
+    print()
